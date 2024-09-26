31de2bd3109fe383467a16037343cac8d3f7c1ec
10yard <jon123wilson@hotmail.com>
2024-09-27 00:05:49 +0100

    amend last

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ rmdir dist /s /Q
 xcopy wolf256 dist\wolf256 /S /i /Y
 rmdir dist\wolf256\cfg /s /Q
 rmdir dist\wolf256\inp /s /Q
-copy readme.txt dist\readme.txt /Y
+copy readme.md dist\readme.md /Y
 
 "C:\Program Files\Python37\Scripts\pyinstaller" dkbros.py --onefile --clean --noconsole --icon dkafe.ico
 "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /tr http://timestamp.digicert.com /n "Open Source Developer" dist\dkbros.exe
